f4404a9a1c50199b496bd0913dd0c1df46fa7188
MikhailovAlexander <vestroy@gmail.com>
2022-09-16 22:52:52 +0500

    add sharp version for the third task

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -3,50 +3,23 @@
     public static class Program
     {
         /// <summary>
-        /// Calculates the greatest common divisor of two numbers
+        /// Generates all permutations by a set of chars
         /// </summary>
-        /// <param name="a">first number</param>
-        /// <param name="b">second number</param>
-        /// <returns>greatest common divisor</returns>
-        public static int GcdRecursive(int a, int b)
+        /// <param name="items">A set of chars</param>
+        /// <exception cref="ArgumentException">Thrown when the chars in items have duplicates</exception>
+        /// <returns>An array of permutation strings</returns>
+        public static string[] GeneratePermutations(char[] items)
         {
             throw new NotImplementedException();
         }
-        /// <summary>
-        /// Calculates the greatest common divisor of two numbers
-        /// </summary>
-        /// <param name="a">first number</param>
-        /// <param name="b">second number</param>
-        /// <returns>greatest common divisor</returns>
-        public static int GcdIterativeSlow(int a, int b)
-        {
-            throw new NotImplementedException();
-        }
-        /// <summary>
-        /// Calculates the greatest common divisor of two numbers
-        /// </summary>
-        /// <param name="a">first number</param>
-        /// <param name="b">second number</param>
-        /// <returns>greatest common divisor</returns>
-        public static int GcdIterativeFast(int a, int b)
-        {
-            throw new NotImplementedException();
-        }
-        /// <summary>
-        /// Calculates the least common multiple of two numbers
-        /// </summary>
-        /// <param name="a">first number</param>
-        /// <param name="b">second number</param>
-        /// <returns>least common multiple</returns>
-        public static int Lcm(int a, int b)
-        {
-            throw new NotImplementedException();
-        }
-        
+
         public static void Main(string[] args)
         {
-            Console.WriteLine($"a = 9\nb = 3\ngcd(a, b) = {GcdIterativeFast(9, 3)}");
-            Console.WriteLine($"a = 6\nb = 8\nlcm(a, b) = {Lcm(6, 8)}");
+            string[] permutations = GeneratePermutations(new char[4] {'a', 'b', 'c', 'd'});
+            foreach (var pmt in permutations)
+            {
+                Console.WriteLine(pmt);
+            }
         }
     }
 }
