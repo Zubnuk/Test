1cdce7c67ba9544fbfb469f1ad53f051e9d5064a
Андрей Пичеев <avpicheev@edu.hse.ru>
2022-09-17 20:22:26 +0500

    task 2

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ def gcd_recursive(a: int, b: int) -> int:
     :except Exception: when a or b value is None
     :return: greatest common divisor
     """
-    pass
+    if a is None or b is None:
+        raise Exception()
+    if a == b: return a
+    if a * b == 0: return a + b
+    if a < b:
+        a, b = b, a
+    return gcd_recursive(a - b, b)
 
 
 def gcd_iterative_slow(a: int, b: int) -> int:
@@ -19,7 +25,13 @@ def gcd_iterative_slow(a: int, b: int) -> int:
     :except Exception: when a or b value is None
     :return: greatest common divisor
     """
-    pass
+    if a is None or b is None:
+        raise Exception()
+    while a != b and a * b != 0:
+        if a < b:
+            a, b = b, a
+        a -= b
+    return a
 
 
 def gcd_iterative_fast(a: int, b: int) -> int:
@@ -31,7 +43,11 @@ def gcd_iterative_fast(a: int, b: int) -> int:
     :except Exception: when a or b value is None
     :return: greatest common divisor
     """
-    pass
+    if a is None or b is None:
+        raise Exception()
+    while b != 0:
+        a, b = b, a % b
+    return a
 
 
 def lcm(a: int, b: int) -> int:
@@ -42,7 +58,9 @@ def lcm(a: int, b: int) -> int:
     :except Exception: when a or b value is None
     :return: the least common multiple
     """
-    pass
+    if a is None or b is None:
+        raise Exception()
+    return int(a * b / gcd_iterative_fast(a, b))
 
 
 def main():
