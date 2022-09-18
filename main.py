f2cfe404d28670e5c0c7b0e18265821aedfb79ff
mirro <mirrordeni@gmail.com>
2022-09-18 13:38:18 +0500

    The functions are written: -finding the greatest common divisor of two numbers in recursive, iterative and fast iterative ways -finding the least common multiple of two numbers using fast finding of the greatest common divisor

--- main.py
+++ main.py
@@ -1,48 +1,80 @@
-def gcd_recursive(a: int, b: int) -> int:
+def gcd_recursive(first_number: int, second_number: int) -> int:
     """Calculates the greatest common divisor of two numbers.
     Recursive implementation.
 
-    :param a: first number
-    :param b: second number
+    :param first_number: first number
+    :param second_number: second number
     :except Exception: when a or b value is None
     :return: greatest common divisor
     """
-    pass
+    if first_number is None or second_number is None:
+        raise Exception("Can't find the greatest common divisor of None.")
+
+    # Base
+    if first_number == second_number:
+        return first_number
+    if first_number * second_number == 0:
+        return first_number + second_number
+
+    if first_number < second_number:
+        first_number, second_number = second_number, first_number
+
+    return gcd_recursive(first_number - second_number, second_number)
 
 
-def gcd_iterative_slow(a: int, b: int) -> int:
+def gcd_iterative_slow(first_number: int, second_number: int) -> int:
     """Calculates the greatest common divisor of two numbers.
     Iterative implementation using subtraction.
 
-    :param a: first number
-    :param b: second number
+    :param first_number: first number
+    :param second_number: second number
     :except Exception: when a or b value is None
     :return: greatest common divisor
     """
-    pass
+    if first_number is None or second_number is None:
+        raise Exception("Can't find the greatest common divisor of None.")
+
+    if first_number == second_number:
+        return first_number
+    if first_number * second_number == 0:
+        return first_number + second_number
+
+    while first_number != second_number and first_number * second_number != 0:
+        if first_number < second_number:
+            first_number, second_number = second_number, first_number
+        first_number -= second_number
+    return first_number
 
 
-def gcd_iterative_fast(a: int, b: int) -> int:
+def gcd_iterative_fast(first_number: int, second_number: int) -> int:
     """Calculates the greatest common divisor of two numbers
     Iterative implementation using division.
 
-    :param a: first number
-    :param b: second number
+    :param first_number: first number
+    :param second_number: second number
     :except Exception: when a or b value is None
     :return: greatest common divisor
     """
-    pass
+    if first_number is None or second_number is None:
+        raise Exception("Can't find the greatest common divisor of None.")
+
+    while second_number:
+        first_number, second_number = second_number, first_number % second_number
+    return first_number
 
 
-def lcm(a: int, b: int) -> int:
+def lcm(first_number: int, second_number: int) -> int:
     """Calculates the least common multiple of two numbers
 
-    :param a: first number
-    :param b: second number
+    :param first_number: first number
+    :param second_number: second number
     :except Exception: when a or b value is None
     :return: the least common multiple
     """
-    pass
+    if first_number is None or second_number is None:
+        raise Exception("Can't find the least common multiple of None.")
+
+    return int((first_number * second_number) / gcd_iterative_fast(first_number, second_number))
 
 
 def main():
