9cbc91913423da5847ddf328813035553b374044
daria-rakovskaia <113023899+daria-rakovskaia@users.noreply.github.com>
2022-09-19 13:54:50 +0500

    dark_blue_team_python_task_3

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
+import math
 import time
 from typing import Any
 
 
-def print_exec_time(func: callable(object), **kwargs: dict[str: Any]) -> None:
+def print_exec_time(func: callable(object), **kwargs) -> None:
     start_time = time.time()
     func(**kwargs)
     print(f'duration: {time.time() - start_time} seconds')
 
 
-def generate_permutations(items: frozenset[Any]) -> list[str]:
+def factorial(a):
+    result = 1
+    for i in range(1, a + 1):
+        result *= i
+    return result
+
+def generate_permutations(items):
     """Generates all permutations by a set of items.
 
     :param items: a frozenset(immutable) with some items.
     :raise Exception: when the items value is None.
     :return: a list with permutation strings.
     """
-    pass
+    arr = list(items)
+    length = len(arr)
+    quant = factorial(length)
+    result = []
+
+    if length == 0:
+        return result
+
+    for i in range(1, quant + 1):
+        s = ""
+        symbols_left = length
+        temp_arr = arr.copy()
+        period = 1
+
+        for j in range(length):
+            period *= symbols_left
+            position = (math.ceil(i * period / quant) - 1) % symbols_left
+            s += str(temp_arr[position])
+            temp_arr.pop(position)
+            symbols_left -= 1
+        result.append(s)
+    return result
 
 
 def main():
