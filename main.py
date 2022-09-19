6aadf4593bfc67a5a1bc7174a9d41bbb5f27d1ba
Daniil Svidler <svidlerdaniil@yandex.ru>
2022-09-19 13:54:47 +0500

    Реализовали рекурсивную функцию перестановок.

--- main.py
+++ main.py
@@ -10,12 +10,22 @@ def print_exec_time(func: callable(object), **kwargs: dict[str: Any]) -> None:
 
 def generate_permutations(items: frozenset[Any]) -> list[str]:
     """Generates all permutations by a set of items.
-
     :param items: a frozenset(immutable) with some items.
     :raise Exception: when the items value is None.
     :return: a list with permutation strings.
     """
-    pass
+    if None in items:
+        raise Exception("встретился None в списке")
+    items = list(items)
+    if len(items) == 1:
+        return [str(items[0])]
+    results = []
+    for item in items:
+        other_items = [i for i in items if i != item]
+        permutations = generate_permutations(frozenset(other_items))
+        for permutation in permutations:
+            results.append(str(item) + str(permutation))
+    return results
 
 
 def main():
