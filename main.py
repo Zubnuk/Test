fd4c72335186bfa1f2b8392b08c0ad74a3fda004
mirro <mirrordeni@gmail.com>
2022-09-25 23:42:46 +0500

    The function of iterative finding of all permutations is implemented

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import copy
 import time
+from math import factorial
 from typing import Any
 
 
@@ -15,7 +17,39 @@ def generate_permutations(items: frozenset[Any]) -> list[str]:
     :raise Exception: when the items value is None.
     :return: a list with permutation strings.
     """
-    pass
+    if items is None:
+        raise Exception("...")
+    if len(items) in (0, 1):
+        answer = list(map(str, items))
+        return answer
+
+    main_array = []
+    for value in items:
+        main_array.append(str(value))
+
+    steps = [[[main_array[0]]]]
+
+    for main_index in range(len(main_array) - 1):
+        new_step = []
+        for _ in range(factorial(main_index + 2)):
+            buffer = copy.deepcopy(steps[main_index][_ % factorial(main_index + 1)])
+            new_step.append(buffer)
+        new_step.sort()
+
+        insert_index = 0
+        for permutation_index in range(len(new_step)):
+            new_step[permutation_index].insert(insert_index, main_array[main_index + 1])
+            insert_index += 1
+            if insert_index == main_index + 2:
+                insert_index = 0
+        insert_index = 0
+
+        steps.append(new_step)
+
+    answer = list(map(''.join, steps[-1]))
+    answer = list(set(answer))
+
+    return answer
 
 
 def main():
