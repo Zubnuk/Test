b755e05efa9c443ddf308cdb916ed29787e4d322
EgorTrukhin <etruhin2000@yandex.ru>
2022-10-02 19:21:37 +0500

    pink_team_python_task_4: completed function get_min_cost_path

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class ArgumentException(Exception):
     """Exception raised for errors in the input parameter.
 
@@ -10,8 +13,11 @@ class ArgumentException(Exception):
         super().__init__(self.message)
 
 
-def get_min_cost_path(price_table: list[list[float]]) ->\
-        dict[str: float, str: list[tuple[int, int]]]:
+COST = 'cost'
+PATH = 'path'
+
+
+def get_min_cost_path(price_table: list[list[float]]) -> dict[str: float, str: list[tuple[int, int]]]:
     """Searches for the minimum cost path in the table. Each cell in the table
     has some price per visit.
     :param price_table: a matrix with float cell price values.
@@ -19,7 +25,68 @@ def get_min_cost_path(price_table: list[list[float]]) ->\
     :return: a dictionary with keys: cost - the minimum value of the cost of the
     path, path - an ordered list of tuples with cell indices.
     """
-    pass
+    exception_message = 'The price table is not a rectangular matrix with float values'
+    if (price_table is None) or (len(price_table) == 0):
+        raise ArgumentException(exception_message)
+
+    row_length = len(price_table[0])
+    for i in price_table:
+        if (i is None) or (len(i) != row_length):
+            raise ArgumentException(exception_message)
+        for j in i:
+            if type(j) is not float:
+                raise ArgumentException(exception_message)
+
+    cpt = __get_completed_price_table__(price_table)  # hereinafter called `cpt`, means completed_price_table
+
+    path = []
+    cpt_row_index = len(cpt) - 1
+    cpt_col_index = len(cpt[0]) - 1
+    cost = cpt[cpt_row_index][cpt_col_index]
+
+    while cpt_row_index > 0 and cpt_col_index > 0:
+        curr_row_idx = cpt_row_index - 1
+        curr_col_idx = cpt_col_index - 1
+
+        path_idx = (curr_row_idx, curr_col_idx)
+        path.insert(0, path_idx)
+        check_higher_value = cpt[curr_row_idx][cpt_col_index] >= cpt[cpt_row_index][curr_col_idx]
+
+        if check_higher_value:
+            cpt_col_index -= 1
+        else:
+            cpt_row_index -= 1
+
+    return {COST: cost, PATH: path}
+
+
+def __get_completed_price_table__(price_table: list[list[float]]) -> list[list[float]]:
+    for i, row in enumerate(price_table):
+        for j in range(len(row)):
+            if (i - 1) < 0 and (j - 1) < 0:
+                continue
+            if (i - 1) < 0 and (j - 1) > -1:
+                price_table[i][j] += price_table[i][j - 1]
+            if (i - 1) > -1 and (j - 1) < 0:
+                price_table[i][j] += price_table[i - 1][j]
+            if (i - 1) >= 0 and (j - 1) >= 0:
+                check_higher_value = price_table[i - 1][j] >= price_table[i][j - 1]
+                if check_higher_value:
+                    price_table[i][j] += price_table[i][j - 1]
+                else:
+                    price_table[i][j] += price_table[i - 1][j]
+
+    max_value_list = []
+    for i in range(len(price_table[0]) + 1):
+        max_value_list.append(sys.float_info.max)
+    price_table.insert(0, max_value_list)
+
+    for i, row in enumerate(price_table):
+        if i == 0:
+            continue
+        row.insert(0, sys.float_info.max)
+
+    return price_table
 
 
 def main():
