3c2c97fc032130754eebffa4235b2a0323635610
MikhailovAlexander <avmikhaylov_3@edu.hse.ru>
2022-10-10 19:58:26 +0500

    add task

---
+++ main.py
@@ -0,0 +1,9 @@
+def get_triangle_path_count(length: int) -> int:
+    pass
+
+
+def main():
+    print(get_triangle_path_count(4))
+
+
+main()
---
+++ test_triangle_path.py
@@ -0,0 +1,42 @@
+import unittest
+from main import get_triangle_path_count
+from castom_exception import ArgumentException
+
+
+class TestTrianglePath(unittest.TestCase):
+    @staticmethod
+    def __calculate_result(length):
+        return 1/3 * 2**length + 2/3 * (-1)**length
+
+    def test_none(self):
+        self.assertRaisesRegex(ArgumentException,
+                               'The parameter length must be an integer '
+                               'greater than 0',
+                               get_triangle_path_count, None)
+
+    def test_zero(self):
+        self.assertRaisesRegex(ArgumentException,
+                               'The parameter length must be an integer '
+                               'greater than 0',
+                               get_triangle_path_count, 0)
+
+    def test_negative(self):
+        self.assertRaisesRegex(ArgumentException,
+                               'The parameter length must be an integer '
+                               'greater than 0',
+                               get_triangle_path_count, -1)
+
+    def test_not_int(self):
+        self.assertRaisesRegex(ArgumentException,
+                               'The parameter length must be an integer '
+                               'greater than 0',
+                               get_triangle_path_count, 1.1)
+
+    def test_triangle_path(self):
+        for i in range(1, 10):
+            self.assertEqual(get_triangle_path_count(i),
+                             TestTrianglePath.__calculate_result(i))
+
+
+if __name__ == '__main__':
+    unittest.main()
