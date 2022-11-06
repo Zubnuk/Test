b4e0fbc222bc879d2199dfd7521dfca64a84f343
MikhailovAlexander <vestroy@gmail.com>
2022-11-07 00:22:32 +0500

    add task

--- main.py
+++ main.py
@@ -13,172 +13,196 @@ DOWNTIME = 'downtime'
 
 
 class _ScheduleTask:
+    """An internal class for the schedule, a wrapper for the Task object,
+    represents information about the time points for the start and end of the
+    task stages.
+
+    Properties
+    ----------
+    task(self) -> Task:
+        Returns a source Task object.
+
+    keys_to_sort(self) -> tuple[int, int]:
+        Returns keys that allow to compare this task with another for sorting
+        task list.
+
+    stage1_start(self) -> int:
+        Returns the first stage start point.
+
+    stage1_start(self, value: int) -> None:
+        Sets the first stage start point.
+
+    stage1_end(self) -> int:
+        Returns the first stage end point.
+
+    stage2_start(self) -> int:
+        Returns the second stage start point.
+
+    stage2_start(self, value: int) -> None:
+        Sets the second stage start point.
+
+    stage2_end(self) -> int:
+        Returns the second stage end point.
+
+    """
     def __init__(self, task: Task):
-        error_msg = _ScheduleTask.__get_param_error(task)
-        if error_msg is not None:
-            raise ScheduleArgumentException(error_msg)
-        self.__task = task
-        self.__stage1_start = None
-        self.__stage2_start = None
+        """_ScheduleTask class constructor to initialize the object.
+
+        :param task: a source task object.
+        :raise ScheduleArgumentException: when the task parameter is not a Task.
+        """
+        pass
 
     @property
     def task(self) -> Task:
-        return self.__task
+        """Returns a source Task object."""
+        pass
 
     @property
     def keys_to_sort(self) -> tuple[int, int]:
-        key1 = int(self.__task.stage1 > self.__task.stage2)
-        key2 = (-1 * self.__task.stage2
-                if self.__task.stage1 > self.__task.stage2
-                else self.__task.stage1)
-        return tuple((key1, key2))
+        """Returns keys that allow to compare this task with another for sorting
+        task list."""
+        pass
 
     @property
     def stage1_start(self) -> int:
-        return self.__stage1_start
+        """Returns the first stage start point."""
+        pass
 
     @stage1_start.setter
     def stage1_start(self, value: int) -> None:
-        if type(value) != int:
-            raise InternalScheduleException(
-                'The stage1 start time is not an integer', self.task)
-        if type(value) != int or value < 0:
-            raise InternalScheduleException(
-                'The stage1 start time is less than 0', self.task)
-        self.__stage1_start = value
+        """Sets the first stage start point.
+
+        :param value: the first stage start point.
+        :raise InternalScheduleException: when the stage1 start time is not an
+            integer, when the stage1 start time is less than 0.
+        :return: None
+        """
+        pass
 
     @property
     def stage1_end(self) -> int:
-        return self.__stage1_start + self.__task.stage1
+        """Returns the first stage end point."""
+        pass
 
     @property
     def stage2_start(self) -> int:
-        return self.__stage2_start
+        """Returns the second stage start point."""
+        pass
 
     @stage2_start.setter
     def stage2_start(self, value: int) -> None:
-        if type(value) != int:
-            raise InternalScheduleException(
-                'The stage2 start time is not an integer', self.task)
-        if value < self.stage1_end:
-            raise InternalScheduleException(
-                'The stage2 start time is less than the stage1 end time',
-                self.task)
-        self.__stage2_start = value
+        """Sets the second stage start point.
+
+        :param value: the second stage start point
+        :raise InternalScheduleException: when the stage2 start time is not an
+            integer, when the stage2 start time is less than the stage1 end
+            time.
+        :return: None
+        """
+        pass
 
     @property
     def stage2_end(self) -> int:
-        return self.__stage2_start + self.__task.stage2
-
-    @staticmethod
-    def __get_param_error(task: Task) -> Union[str, None]:
-        if type(task) != Task:
-            return 'The task parameter is not a Task'
-        return None
+        """Returns the second stage end point."""
+        pass
 
 
 class Schedule:
-    def __init__(self, tasks: list[Task]):
-        error_msg = Schedule.__get_param_error(tasks)
-        if error_msg is not None:
-            raise ScheduleArgumentException(error_msg)
-        self.__tasks = [_ScheduleTask(task) for task in tasks]
-        self.__tasks.sort(key=lambda task: task.keys_to_sort)
-        self.__set_task_times()
+    """A class for calculating the optimal schedule for a conveyor problem with
+    two-stage tasks and two executors.
 
-    @property
-    def task_count(self) -> int:
-        return len(self.__tasks)
+    Properties
+    ----------
+    tasks(self) -> tuple[Task]:
+        Returns the source tasks in the optimal order.
+
+    tasks_names(self) -> tuple[str]:
+        Returns the source task names in the optimal order.
+
+    task_count(self) -> int:
+        Returns the source task count.
+
+    total_duration(self) -> int:
+        Returns the duration of all tasks in the schedule.
+
+    stage1_downtime(self) -> int:
+        Returns the duration of downtime for the first stage in the
+        schedule.
+
+    stage2_downtime(self) -> int:
+        Returns the duration of downtime for the second stage in the
+        schedule.
+
+    total_downtime(self) -> int:
+        Returns the duration of downtime for all stages in the schedule.
+
+    stage1_schedule(self) -> str:
+        Returns a schedule for the first stage containing time points for
+        the start and end of tasks and downtime periods.
+
+    stage2_schedule(self) -> str:
+        Returns a schedule for the second stage containing time points for
+        the start and end of tasks and downtime periods.
+    """
+    def __init__(self, tasks: list[Task]):
+        """Schedule class constructor to initialize the object.
+
+        :param tasks: a source task list.
+        :raise ScheduleArgumentException: when the tasks parameter is not a
+            list, when the task list is empty, when the task list contains
+            not a Task object.
+        """
+        pass
 
     @property
     def tasks(self) -> tuple[Task]:
-        return tuple([ord_task.task for ord_task in self.__tasks])
+        """Returns the source tasks in the optimal order."""
+        pass
 
     @property
     def tasks_names(self) -> tuple[str]:
-        return tuple([ord_task.task.name for ord_task in self.__tasks])
+        """Returns the source task names in the optimal order."""
+        pass
+
+    @property
+    def task_count(self) -> int:
+        """Returns the source task count."""
+        pass
 
     @property
     def total_duration(self) -> int:
-        return self.__tasks[-1].stage2_end
+        """Returns the duration of all tasks in the schedule."""
+        pass
 
     @property
     def stage1_downtime(self) -> int:
-        return self.total_duration - self.__tasks[-1].stage1_end
+        """Returns the duration of downtime for the first stage in the
+        schedule."""
+        pass
 
     @property
     def stage2_downtime(self) -> int:
-        downtime = self.__tasks[0].task.stage1
-        for task_idx in range(1, self.task_count):
-            downtime += self.__tasks[task_idx].stage2_start - \
-                        self.__tasks[task_idx - 1].stage2_end
-        return downtime
+        """Returns the duration of downtime for the second stage in the
+        schedule."""
+        pass
 
     @property
     def total_downtime(self) -> int:
-        return self.stage1_downtime + self.stage2_downtime
+        """Returns the duration of downtime for all stages in the schedule."""
+        pass
 
     @property
-    def conveyor1_schedule(self) -> str:
-        return Schedule.__get_string_schedule(self.__get_conveyor1_schedule())
+    def stage1_schedule(self) -> str:
+        """Returns a schedule for the first stage containing time points for
+        the start and end of tasks and downtime periods."""
+        pass
 
     @property
-    def conveyor2_schedule(self) -> str:
-        return Schedule.__get_string_schedule(self.__get_conveyor2_schedule())
-
-    def __set_task_times(self):
-        first_task = self.__tasks[0]
-        first_task.stage1_start = 0
-        first_task.stage2_start = first_task.stage1_end
-        for task_idx in range(1, self.task_count):
-            cur_task = self.__tasks[task_idx]
-            prev_task = self.__tasks[task_idx - 1]
-            cur_task.stage1_start = prev_task.stage1_end
-            cur_task.stage2_start = max(cur_task.stage1_end,
-                                        prev_task.stage2_end)
-
-    def __get_conveyor1_schedule(self):
-        schedule = [{TASK: task.task.name, START: task.stage1_start,
-                     END: task.stage1_end} for task in self.__tasks]
-        if self.stage1_downtime != 0:
-            schedule.append({TASK: DOWNTIME,
-                             START: self.total_duration - self.stage1_downtime,
-                             END: self.total_duration})
-        return schedule
-
-    def __get_conveyor2_schedule(self):
-        first_task = self.__tasks[0]
-        schedule = [{TASK: DOWNTIME,
-                    START: first_task.stage1_start,
-                    END: first_task.stage1_end}]
-        for task_idx in range(1, self.task_count):
-            cur_task = self.__tasks[task_idx]
-            prev_task = self.__tasks[task_idx - 1]
-            if cur_task.stage2_start != prev_task.stage2_end:
-                schedule.append({TASK: DOWNTIME, START: prev_task.stage2_end,
-                                 END: cur_task.stage2_start})
-            schedule.append({TASK: cur_task.task.name,
-                             START: cur_task.stage2_start,
-                             END: cur_task.stage2_end})
-        return schedule
-
-    @staticmethod
-    def __get_string_schedule(schedule):
-        lines = [f'{idx}. task: {line[TASK]} from: {line[START]} to {line[END]}'
-                 for idx, line in enumerate(schedule)]
-        return '\n'.join(lines)
-
-    @staticmethod
-    def __get_param_error(tasks: list[Task]) -> Union[str, None]:
-        if type(tasks) != list:
-            return 'The task parameter is not a list'
-        if len(tasks) < 1:
-            return 'The task list is empty'
-        for idx, value in enumerate(tasks):
-            if type(value) != Task:
-                return 'The task list contains not a Task object ' \
-                       f'in the position {idx}'
-        return None
+    def stage2_schedule(self) -> str:
+        """Returns a schedule for the second stage containing time points for
+        the start and end of tasks and downtime periods."""
+        pass
 
 
 def main():
@@ -187,11 +211,11 @@ def main():
     sched = Schedule(tasks)
     print(f'optimal order of execution of tasks: {sched.tasks_names}')
     print(f'minimum task completion time: {sched.total_duration}')
-    print(f'downtime of the first conveyor: {sched.stage1_downtime}')
-    print(f'downtime of the second conveyor: {sched.stage2_downtime}')
-    print(f'total downtime of conveyors:{sched.total_downtime}')
-    print(f'\nthe first conveyor schedule:\n{sched.conveyor1_schedule}')
-    print(f'\nthe second conveyor schedule:\n{sched.conveyor2_schedule}')
+    print(f'downtime of the first stage: {sched.stage1_downtime}')
+    print(f'downtime of the second stage: {sched.stage2_downtime}')
+    print(f'total downtime:{sched.total_downtime}')
+    print(f'\nthe first stage schedule:\n{sched.stage1_schedule}')
+    print(f'\nthe second stage schedule:\n{sched.stage2_schedule}')
 
 
 if __name__ == '__main__':
---
+++ tests/test_schedule.py
@@ -0,0 +1,212 @@
+import unittest
+
+
+from main import Schedule, ScheduleArgumentException
+from task import Task
+
+
+class TestSchedule(unittest.TestCase):
+    def test_none_tasks(self):
+        self.assertRaisesRegex(ScheduleArgumentException,
+                               'Error during initialization of the Schedule '
+                               'object! The tasks parameter is not a list',
+                               Schedule.__init__, None, None)
+
+    def test_empty_tasks(self):
+        self.assertRaisesRegex(ScheduleArgumentException,
+                               'Error during initialization of the Schedule '
+                               'object! The task list is empty',
+                               Schedule.__init__, None, [])
+
+    def test_tasks_contains_non_task(self):
+        tasks = [Task('a', 7, 2), Task('b', 3, 4), 'non task object']
+        self.assertRaisesRegex(ScheduleArgumentException,
+                               'Error during initialization of the Schedule '
+                               'object! The task list contains not a Task '
+                               'object in the position 2',
+                               Schedule.__init__, None, tasks)
+
+    def test_single(self):
+        task_a = Task('a', 1, 2)
+        sched = Schedule([task_a])
+        stage1_schedule = '1. task: a from: 0 to 1\n' \
+                          '2. task: downtime from: 1 to 3'
+        stage2_schedule = '1. task: downtime from: 0 to 1\n' \
+                          '2. task: a from: 1 to 3'
+        self.assertEqual(tuple([task_a]), sched.tasks)
+        self.assertEqual(tuple('a'), sched.tasks_names)
+        self.assertEqual(1, sched.task_count)
+        self.assertEqual(3, sched.total_duration)
+        self.assertEqual(2, sched.stage1_downtime)
+        self.assertEqual(1, sched.stage2_downtime)
+        self.assertEqual(3, sched.total_downtime)
+        self.assertEqual(stage1_schedule, sched.stage1_schedule)
+        self.assertEqual(stage2_schedule, sched.stage2_schedule)
+
+    def test_double(self):
+        task_a = Task('a', 2, 1)
+        task_b = Task('b', 1, 2)
+        sched = Schedule([task_a, task_b])
+        stage1_schedule = '1. task: b from: 0 to 1\n' \
+                          '2. task: a from: 1 to 3\n' \
+                          '3. task: downtime from: 3 to 4'
+        stage2_schedule = '1. task: downtime from: 0 to 1\n' \
+                          '2. task: b from: 1 to 3\n' \
+                          '3. task: a from: 3 to 4'
+        self.assertEqual(tuple([task_b, task_a]), sched.tasks)
+        self.assertEqual(tuple(['b', 'a']), sched.tasks_names)
+        self.assertEqual(2, sched.task_count)
+        self.assertEqual(4, sched.total_duration)
+        self.assertEqual(1, sched.stage1_downtime)
+        self.assertEqual(1, sched.stage2_downtime)
+        self.assertEqual(2, sched.total_downtime)
+        self.assertEqual(stage1_schedule, sched.stage1_schedule)
+        self.assertEqual(stage2_schedule, sched.stage2_schedule)
+
+    def test_triple_stage2_greater_only(self):
+        task_a = Task('a', 1, 2)
+        task_b = Task('b', 3, 4)
+        task_c = Task('c', 5, 6)
+        sched = Schedule([task_a, task_b, task_c])
+        stage1_schedule = '1. task: a from: 0 to 1\n' \
+                          '2. task: b from: 1 to 4\n' \
+                          '3. task: c from: 4 to 9\n' \
+                          '4. task: downtime from: 9 to 15'
+        stage2_schedule = '1. task: downtime from: 0 to 1\n' \
+                          '2. task: a from: 1 to 3\n' \
+                          '3. task: downtime from: 3 to 4\n' \
+                          '4. task: b from: 4 to 8\n' \
+                          '5. task: downtime from: 8 to 9\n' \
+                          '6. task: c from: 9 to 15'
+        self.assertEqual(tuple([task_a, task_b, task_c]), sched.tasks)
+        self.assertEqual(tuple(['a', 'b', 'c']), sched.tasks_names)
+        self.assertEqual(3, sched.task_count)
+        self.assertEqual(15, sched.total_duration)
+        self.assertEqual(6, sched.stage1_downtime)
+        self.assertEqual(3, sched.stage2_downtime)
+        self.assertEqual(9, sched.total_downtime)
+        self.assertEqual(stage1_schedule, sched.stage1_schedule)
+        self.assertEqual(stage2_schedule, sched.stage2_schedule)
+
+    def test_triple_stage2_less_only(self):
+        task_a = Task('a', 2, 1)
+        task_b = Task('b', 4, 3)
+        task_c = Task('c', 6, 5)
+        sched = Schedule([task_a, task_b, task_c])
+        stage1_schedule = '1. task: c from: 0 to 6\n' \
+                          '2. task: b from: 6 to 10\n' \
+                          '3. task: a from: 10 to 12\n' \
+                          '4. task: downtime from: 12 to 15'
+        stage2_schedule = '1. task: downtime from: 0 to 6\n' \
+                          '2. task: c from: 6 to 11\n' \
+                          '3. task: b from: 11 to 14\n' \
+                          '4. task: a from: 14 to 15'
+        self.assertEqual(tuple([task_c, task_b, task_a]), sched.tasks)
+        self.assertEqual(tuple(['c', 'b', 'a']), sched.tasks_names)
+        self.assertEqual(3, sched.task_count)
+        self.assertEqual(15, sched.total_duration)
+        self.assertEqual(3, sched.stage1_downtime)
+        self.assertEqual(6, sched.stage2_downtime)
+        self.assertEqual(9, sched.total_downtime)
+        self.assertEqual(stage1_schedule, sched.stage1_schedule)
+        self.assertEqual(stage2_schedule, sched.stage2_schedule)
+
+    def test_triple_mix(self):
+        task_a = Task('a', 2, 1)
+        task_b = Task('b', 3, 4)
+        task_c = Task('c', 6, 5)
+        sched = Schedule([task_a, task_b, task_c])
+        stage1_schedule = '1. task: b from: 0 to 3\n' \
+                          '2. task: c from: 3 to 9\n' \
+                          '3. task: a from: 9 to 11\n' \
+                          '4. task: downtime from: 11 to 15'
+        stage2_schedule = '1. task: downtime from: 0 to 3\n' \
+                          '2. task: b from: 3 to 7\n' \
+                          '3. task: downtime from: 7 to 9\n' \
+                          '4. task: c from: 9 to 14\n' \
+                          '5. task: a from: 14 to 15'
+        self.assertEqual(tuple([task_b, task_c, task_a]), sched.tasks)
+        self.assertEqual(tuple(['b', 'c', 'a']), sched.tasks_names)
+        self.assertEqual(3, sched.task_count)
+        self.assertEqual(15, sched.total_duration)
+        self.assertEqual(4, sched.stage1_downtime)
+        self.assertEqual(5, sched.stage2_downtime)
+        self.assertEqual(9, sched.total_downtime)
+        self.assertEqual(stage1_schedule, sched.stage1_schedule)
+        self.assertEqual(stage2_schedule, sched.stage2_schedule)
+
+    def test_pentad(self):
+        task_a = Task('a', 4, 3)
+        task_b = Task('b', 5, 2)
+        task_c = Task('c', 3, 5)
+        task_d = Task('d', 2, 3)
+        task_e = Task('e', 4, 4)
+        sched = Schedule([task_a, task_b, task_c, task_d, task_e])
+        ordered_tasks = tuple([task_d, task_c, task_e, task_a, task_b])
+        stage1_schedule = '1. task: d from: 0 to 2\n' \
+                          '2. task: c from: 2 to 5\n' \
+                          '3. task: e from: 5 to 9\n' \
+                          '4. task: a from: 9 to 13\n' \
+                          '5. task: b from: 13 to 18\n' \
+                          '6. task: downtime from: 18 to 20'
+        stage2_schedule = '1. task: downtime from: 0 to 2\n' \
+                          '2. task: d from: 2 to 5\n' \
+                          '3. task: c from: 5 to 10\n' \
+                          '4. task: e from: 10 to 14\n' \
+                          '5. task: a from: 14 to 17\n' \
+                          '6. task: downtime from: 17 to 18\n' \
+                          '7. task: b from: 18 to 20'
+        self.assertEqual(ordered_tasks, sched.tasks)
+        self.assertEqual(tuple(['d', 'c', 'e', 'a', 'b']), sched.tasks_names)
+        self.assertEqual(5, sched.task_count)
+        self.assertEqual(20, sched.total_duration)
+        self.assertEqual(2, sched.stage1_downtime)
+        self.assertEqual(3, sched.stage2_downtime)
+        self.assertEqual(5, sched.total_downtime)
+        self.assertEqual(stage1_schedule, sched.stage1_schedule)
+        self.assertEqual(stage2_schedule, sched.stage2_schedule)
+
+    def test_sevenfold(self):
+        task_a = Task('a', 7, 2)
+        task_b = Task('b', 3, 4)
+        task_c = Task('c', 2, 5)
+        task_d = Task('d', 4, 1)
+        task_e = Task('e', 6, 6)
+        task_f = Task('f', 5, 3)
+        task_g = Task('g', 4, 5)
+        sched = Schedule([task_a, task_b, task_c, task_d, task_e, task_f,
+                          task_g])
+        ordered_tasks = tuple([task_c, task_b, task_g, task_e, task_f, task_a,
+                               task_d])
+        stage1_schedule = '1. task: c from: 0 to 2\n' \
+                          '2. task: b from: 2 to 5\n' \
+                          '3. task: g from: 5 to 9\n' \
+                          '4. task: e from: 9 to 15\n' \
+                          '5. task: f from: 15 to 20\n' \
+                          '6. task: a from: 20 to 27\n' \
+                          '7. task: d from: 27 to 31\n' \
+                          '8. task: downtime from: 31 to 32'
+        stage2_schedule = '1. task: downtime from: 0 to 2\n' \
+                          '2. task: c from: 2 to 7\n' \
+                          '3. task: b from: 7 to 11\n' \
+                          '4. task: g from: 11 to 16\n' \
+                          '5. task: e from: 16 to 22\n' \
+                          '6. task: f from: 22 to 25\n' \
+                          '7. task: downtime from: 25 to 27\n' \
+                          '8. task: a from: 27 to 29\n' \
+                          '9. task: downtime from: 29 to 31\n' \
+                          '10. task: d from: 31 to 32'
+        self.assertEqual(ordered_tasks, sched.tasks)
+        self.assertEqual(tuple(['c', 'b', 'g', 'e', 'f', 'a', 'd']),
+                         sched.tasks_names)
+        self.assertEqual(7, sched.task_count)
+        self.assertEqual(32, sched.total_duration)
+        self.assertEqual(1, sched.stage1_downtime)
+        self.assertEqual(6, sched.stage2_downtime)
+        self.assertEqual(7, sched.total_downtime)
+        self.assertEqual(stage1_schedule, sched.stage1_schedule)
+        self.assertEqual(stage2_schedule, sched.stage2_schedule)
+
+
+if __name__ == '__main__':
+    unittest.main()
