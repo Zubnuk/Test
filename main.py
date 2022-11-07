7de275c806d3caf021e5a0ce1c4280a2cc268369
Gleb <tomlenov16@gmail.com>
2022-11-07 22:15:51 +0500

    .

--- main.py
+++ main.py
@@ -1,13 +1,62 @@
 from custom_exception import ArgumentException
 
 
-def tridiagonal_determinant(matrix: list[list[int]]) -> int:
-    """Calculates the value of the tridiagonal matrix determinant
-    :param matrix: an integer tridiagonal square matrix
-    :raise ArgumentException: when parameter is not a tridiagonal integer matrix
-    :return: the value of the matrix determinant
-    """
-    pass
+def is_tridiagonal(matrix: [[int]]) -> bool:
+    if matrix is None:  # None
+        return False
+
+    if len(matrix) == 0:  # len = 0
+        return False
+
+    for i in range(len(matrix)):  # not rectangle
+        if len(matrix[i]) != len(matrix):
+            return False
+    for i in range(len(matrix)):  # main diag
+        if matrix[i][i] != matrix[0][0]:
+            return False
+    if len(matrix) < 3:
+        return True
+    for i in range(len(matrix) - 1):  # low diag
+        if matrix[i + 1][i] != matrix[1][0]:
+            return False
+        for j in range(i - 1):
+            if matrix[i + 1][j] != 0 and i > 1:
+                return False
+    for i in range(len(matrix) - 1):  # high diag
+        if matrix[i][i + 1] != matrix[0][1]:
+            return False
+        for j in range(i - 1):
+            if matrix[j][i + 1] != 0 and i > 1:
+                return False
+    return True
+
+
+def recur_det(matrix: [[int]]) -> int:
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    if len(matrix) > 2:
+        det = matrix[0][0] * recur_det(str_col_remover(matrix, 1)) - matrix[0][1] * matrix[1][0] * recur_det(str_col_remover(matrix, 2))
+        return det
+
+
+def str_col_remover(matrix: [[int]], num: int) -> [[int]]:
+    matr = [[]]
+    matr.pop()
+    for i in range(num, len(matrix)):
+        matr.append([])
+        for j in range(num, len(matrix)):
+            matr[-1].append(matrix[i][j])
+    return matr
+
+
+def tridiagonal_determinant(matrix: [[int]]) -> int:
+    if not is_tridiagonal(matrix):
+        raise ArgumentException('parameter is not a tridiagonal integer matrix')
+    return recur_det(matrix)
 
 
 def main():
@@ -16,7 +65,3 @@ def main():
               [0, 5, 2, -3],
               [0, 0, 5, 2]]
     print(tridiagonal_determinant(matrix))
-
-
-if __name__ == '__main__':
-    main()
