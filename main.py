cdd8e3253cfd4364508b6fc8a9416606cd3b64ec
nikch <nik.chernov2002@gmail.com>
2022-11-07 18:02:23 +0500

    dark_blue_team_python_task_6

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from custom_exception import ArgumentException
 
 
-def checkerDiagonals(a:int, b:int, c:int,matr:list[list[int]])->bool:
+def detByValue(a: int, b: int, c: int, n: int):
+    if n == 2:
+        return a * a - b * c
+    if n == 1:
+        return a
+    return a * detByValue(a, b, c, n - 1) - b * c * detByValue(a, b, c, n - 2)
+
+
+def checkerDiagonals(a: int, b: int, c: int, matr: list[list[int]]):
     if len(matr) == 2:
-        if matr[0][0] == a and matr[0][1] == b and matr[1][0] == c:
-            return True
+        if matr[0][0] != a or matr[0][1] != b or matr[1][0] != c:
+            raise ArgumentException('parameter is not a tridiagonal integer matrix')
         else:
-            return False
+            return
+
     if matr[0][0] != a or matr[1][0] != c or matr[0][1] != b:
-        return False
-    if matr[0][2:].count(0) != len(matr[0][2:]):
-        return False
+        raise ArgumentException('parameter is not a tridiagonal integer matrix')
+    if matr[0][2:].count(0) != len(
+            matr[0][2:]):  # Если количество 0 не равно длине, то на горизонтальной линии есть что-то, кроме 0
+        raise ArgumentException('parameter is not a tridiagonal integer matrix')
     firstColumn = [i[0] for i in matr[2:]]
     if firstColumn.count(0) != len(firstColumn):
-        return False
+        raise ArgumentException('parameter is not a tridiagonal integer matrix')
     newMatr = [i[1:] for i in matr[1:]]
-    return checkerDiagonals(a, b, c, newMatr)
-
-
+    checkerDiagonals(a, b, c, newMatr)
 
 
 def checker(tridiagonalMatrix: list[list[int]]):
@@ -36,26 +44,12 @@ def checker(tridiagonalMatrix: list[list[int]]):
                 raise ArgumentException('parameter is not a tridiagonal integer matrix')
 
 
-
-
 def tridiagonal_determinant(matrix: list[list[int]]) -> int:
     checker(matrix)
     if len(matrix) == 1:
         return matrix[0][0]
-    if not checkerDiagonals(matrix[0][0], matrix[0][1], matrix[1][0],matrix):
-        raise ArgumentException('parameter is not a tridiagonal integer matrix')
-    if len(matrix) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[1][0]*matrix[0][1]
-    a = matrix[0][0]
-    b = matrix[0][1]
-    c = matrix[1][0]
-    matrMinus2 = matrix[2:]
-    matrMinus2 = [i[2:] for i in matrMinus2]
-    matrMinus1 = matrix[1:]
-    matrMinus1 = [i[1:] for i in matrMinus1]
-    return a*tridiagonal_determinant(matrMinus1) - b*c*tridiagonal_determinant(matrMinus2)
-
-
+    checkerDiagonals(matrix[0][0], matrix[0][1], matrix[1][0], matrix)
+    return detByValue(matrix[0][0], matrix[0][1], matrix[1][0], len(matrix))
 
     """Calculates the value of the tridiagonal matrix determinant
     :param matrix: an integer tridiagonal square matrix
@@ -67,9 +61,9 @@ def tridiagonal_determinant(matrix: list[list[int]]) -> int:
 
 def main():
     matrix = [[2, -3, 0, 0],
-              [1, 2, -3, 0],
-              [0, 1, 2, -3],
-              [0, 0, 1, 2]]
+              [5, 2, -3, 0],
+              [0, 5, 2, -3],
+              [0, 0, 5, 2]]
     print(tridiagonal_determinant(matrix))
 
 
