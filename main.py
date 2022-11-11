affaf0d1e4444d62bd1877f80e6577f03dde0560
Gleb <tomlenov16@gmail.com>
2022-11-11 21:21:05 +0500

    .

--- main.py
+++ main.py
@@ -31,15 +31,15 @@ def is_tridiagonal(matrix: [[int]]) -> bool:
     return True
 
 
-def recur_det(matrix: [[int]]) -> int:
-    if len(matrix) == 1:
-        return matrix[0][0]
+def recur_det(a, b, c, n) -> int:
+    if n == 1:
+        return a
 
-    if len(matrix) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    if n == 2:
+        return a * a - b * c
 
-    if len(matrix) > 2:
-        det = matrix[0][0] * recur_det(str_col_remover(matrix, 1)) - matrix[0][1] * matrix[1][0] * recur_det(str_col_remover(matrix, 2))
+    if n > 2:
+        det = a * recur_det(a, b, c, n - 1) - b * c * recur_det(a, b, c, n - 2)
         return det
 
 
@@ -56,7 +56,14 @@ def str_col_remover(matrix: [[int]], num: int) -> [[int]]:
 def tridiagonal_determinant(matrix: [[int]]) -> int:
     if not is_tridiagonal(matrix):
         raise ArgumentException('parameter is not a tridiagonal integer matrix')
-    return recur_det(matrix)
+    if len(matrix) > 1:
+        a = matrix[0][0]
+        b = matrix[1][0]
+        c = matrix[0][1]
+        n = len(matrix)
+        return recur_det(a, b, c, n)
+    else:
+        return matrix[0][0]
 
 
 def main():
