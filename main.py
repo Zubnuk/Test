f9784e163e6edc74dc2b1b6001f574aaac4fd319
andrejsmirnov <aosmirnov_4@edu.hse.ru>
2022-11-13 14:18:06 +0500

    ver_1

--- main.py
+++ main.py
@@ -7,7 +7,33 @@ def tridiagonal_determinant(matrix: list[list[int]]) -> int:
     :raise ArgumentException: when parameter is not a tridiagonal integer matrix
     :return: the value of the matrix determinant
     """
-    pass
+    exception_message = 'parameter is not a tridiagonal integer matrix'
+    if matrix is None:
+        raise ArgumentException(exception_message)
+    if len(matrix) == 0:
+        raise ArgumentException(exception_message)
+    if len(matrix[0]) != len(matrix):
+        raise ArgumentException(exception_message)
+    for i in range(len(matrix) - 1):
+        dl = len(matrix[i])
+        if dl != len(matrix[i + 1]):
+            raise ArgumentException(exception_message)
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    a = matrix[0][0]
+    b = matrix[0][1]
+    c = matrix[1][0]
+    n = len(matrix)
+
+    def determinant(a, b, c, n):
+        if n == 2:
+            return a * a - b * c
+        if n == 1:
+            return a
+        return a * determinant(a, b, c, n - 1) - b * c * determinant(a, b, c, n - 2)
+
+    return determinant(a, b, c, n)
 
 
 def main():
