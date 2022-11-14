1a4bb37c7a0b7bdd318d26a96638a6d2803328c7
Ilya Tyulin <tyulin600@gmail.com>
2022-11-14 20:03:55 +0500

    Реализовали генерацию строк двумя рекурсивными функциями и подсчет количества маршрутов

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
 from custom_exception import ArgumentException
 
 
+def a(n):
+    if n == 1:
+        return 0
+    if n == 2:
+        return 2
+    return b(n - 1) + c(n - 1)
+
+
+def b(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    return a(n - 1) + c(n - 1)
+
+
+def c(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    return a(n - 1) + b(n - 1)
+
+
+def set0(n):
+    if n == 1:
+        return ['0']
+    strings = set1(n - 1)
+    for x in range(len(strings)):
+        strings[x] = '0' + strings[x]
+    return strings
+
+
+def set1(n):
+    if n == 1:
+        return ['1']
+    strings = set1(n - 1) + set0(n - 1)
+    for x in range(len(strings)):
+        strings[x] = '1' + strings[x]
+    return strings
+
+
 def get_triangle_path_count(length: int) -> int:
     """Calculates the number of closed routes of a target length between three
     vertices A, B and C that start and end at the A vertex. Аll paths between
@@ -10,7 +52,9 @@ def get_triangle_path_count(length: int) -> int:
     greater than 0
     :return: the number of routes.
     """
-    pass
+    if type(length) is not int or length <= 0:
+        raise ArgumentException('The parameter length must be an integer greater than 0')
+    return a(length)
 
 
 def generate_strings(length: int) -> list[str]:
@@ -20,12 +64,14 @@ def generate_strings(length: int) -> list[str]:
     :raise ArgumentException: when integer is not equal or greater than zero.
     :return: the list of strings consisting zeroes and ones.
     """
-    pass
+    if type(length) is not int or length <= 0:
+        raise ArgumentException('The parameter length must be an integer greater than 0')
+    return set0(length) + set1(length)
 
 
 def main():
     print(get_triangle_path_count(4))
-    print(generate_strings(5))
+    print(generate_strings(4))
 
 
 if __name__ == '__main__':
