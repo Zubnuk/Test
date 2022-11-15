29a6c7b8a399672ba71ba042069616852b05970c
John Doe <chepushtanovdima@yandex.ru>
2022-11-15 20:41:25 +0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
 from custom_exception import ArgumentException
 
 
+def Graph_a(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 2
+    return Graph_b(n - 1) + Graph_C(n - 1)
+
+
+def Graph_b(n):
+    if n == 1 or n == 1:
+        return 1
+    return Graph_a(n - 1) + Graph_C(n - 1)
+
+
+def Graph_C(n):
+    if n == 1 or n == 1:
+        return 1
+    return Graph_a(n - 1) + Graph_b(n - 1)
+
+
+def get_zero_in_string(n):
+    if n == 1:
+        return ['0']
+    text = get_one_in_string(n - 1)
+    for i in range(len(text)):
+        text[i] = "0" + text[i]
+    return text
+
+
+def get_one_in_string(n):
+    if n == 1:
+        return ['1']
+    text = get_one_in_string(n-1)+get_zero_in_string(n-1)
+    for i in range(len(text)):
+        text[i] = "1" + text[i]
+    return text
+
+
 def get_triangle_path_count(length: int) -> int:
     """Calculates the number of closed routes of a target length between three
     vertices A, B and C that start and end at the A vertex. Аll paths between
@@ -10,7 +48,11 @@ def get_triangle_path_count(length: int) -> int:
     greater than 0
     :return: the number of routes.
     """
-    pass
+    if type(length) is not int:
+        raise ArgumentException('The parameter length must be an integer greater than 0')
+    elif length <= 0:
+        raise ArgumentException('The parameter length must be an integer greater than 0')
+    return Graph_a(length)
 
 
 def generate_strings(length: int) -> list[str]:
@@ -20,7 +62,11 @@ def generate_strings(length: int) -> list[str]:
     :raise ArgumentException: when integer is not equal or greater than zero.
     :return: the list of strings consisting zeroes and ones.
     """
-    pass
+    if type(length) is not int:
+        raise ArgumentException('The parameter length must be an integer greater than 0')
+    elif length <= 0:
+        raise ArgumentException('The parameter length must be an integer greater than 0')
+    return get_one_in_string(length) + get_zero_in_string(length)
 
 
 def main():
