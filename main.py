b30ae9dcf6169b2f366319f57b7d60c03ba8e929
mirro <mirrordeni@gmail.com>
2022-11-21 00:04:15 +0500

    Program solution.

--- main.py
+++ main.py
@@ -10,7 +10,14 @@ def get_triangle_path_count(length: int) -> int:
     greater than 0
     :return: the number of routes.
     """
-    pass
+    check(length)
+    return a_amount(length)
+
+
+def check(length):
+    if length is None or str(length).isdigit() is False or length <= 0:
+        raise ArgumentException('The parameter length must be an '
+                                'integer greater than 0')
 
 
 def generate_strings(length: int) -> list[str]:
@@ -20,7 +27,45 @@ def generate_strings(length: int) -> list[str]:
     :raise ArgumentException: when integer is not equal or greater than zero.
     :return: the list of strings consisting zeroes and ones.
     """
-    pass
+    check(length)
+    return list(set(set0("", length)+set1("", length)))
+
+
+def set1(string, num):
+    if num <= len(string):
+        arr = []
+        arr.append(string)
+        return arr
+    return set1(string + "1", num) + set0(string + "1", num)
+
+
+def set0(string, num):
+    if num <= len(string):
+        arr = []
+        arr.append(string)
+        return arr
+    return set1(string + "0", num)
+
+
+def a_amount(num):
+    if num == 0:
+        return 1
+    else:
+        return b_amount(num - 1) + c_amount(num - 1)
+
+
+def b_amount(num):
+    if num <= 0:
+        return 0
+    else:
+        return a_amount(num - 1) + c_amount(num - 1)
+
+
+def c_amount(num):
+    if num <= 0:
+        return 0
+    else:
+        return b_amount(num - 1) + a_amount(num - 1)
 
 
 def main():
