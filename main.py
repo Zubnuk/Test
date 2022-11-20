1b3323c0fbdc02e22bb4915c09c91a1489f20179
Gleb <tomlenov16@gmail.com>
2022-11-20 19:58:34 +0500

    .

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ def get_triangle_path_count(length: int) -> int:
     greater than 0
     :return: the number of routes.
     """
-    pass
+    if not isinstance(length, int) or length < 1:
+        raise ArgumentException('The parameter length must be an integer greater than 0')
+    return point_a(length)
 
 
 def generate_strings(length: int) -> list[str]:
@@ -20,7 +22,60 @@ def generate_strings(length: int) -> list[str]:
     :raise ArgumentException: when integer is not equal or greater than zero.
     :return: the list of strings consisting zeroes and ones.
     """
-    pass
+    if length is None or not isinstance(length, int) or length < 1:
+        raise ArgumentException('The parameter length must be an integer greater than 0')
+    itog = []
+    __set_one("", length, itog)
+    __set_zero("", length, itog)
+    return itog
+
+
+def __set_zero(st, n, mas):
+    if n == 1:
+        st = st + '0'
+        mas.append(st)
+        return
+    if st == "":
+        st = st + '0'
+        __set_one(st, n - 1, mas)
+    elif st[-1] != '0':
+        st = st + '0'
+        __set_one(st, n - 1, mas)
+
+
+def __set_one(st, n, mas):
+    if n == 1:
+        st = st + '1'
+        mas.append(st)
+        return
+    else:
+        st = st + '1'
+        __set_zero(st, n - 1, mas)
+        __set_one(st, n - 1, mas)
+
+
+def point_a(n):
+    if n == 1:
+        return 0
+    if n == 2:
+        return 2
+    return point_b(n - 1) + point_c(n - 1)
+
+
+def point_b(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    return point_a(n - 1) + point_c(n - 1)
+
+
+def point_c(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    return point_a(n - 1) + point_b(n - 1)
 
 
 def main():
