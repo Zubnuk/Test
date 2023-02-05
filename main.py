98b1043954b7b2a637096f706c22fe6181668c85
MikhailovAlexander <vestroy@gmail.com>
2023-02-05 16:02:45 +0500

    add task

--- main.py
+++ main.py
@@ -1,60 +1,31 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+def get_win_sequence(input_string: str) -> str:
+    """Selects a subsequence from the source sequence for the task 1.
 
-from schedule_graph import ScheduleGraph
-from schedule_checker import ScheduleChecker
-from schedule import Schedule
+    :param input_string: the first line contains the number N - the length of
+    the source sequence. The second line contains a sequence of uppercase
+    Latin letters.
+    :return: a subsequence from the source sequence.
+    """
+    pass
+
+
+def get_water_volume(input_string: str) -> int:
+    """Сalculates the number of blocks filled with water for the task 2.
+
+    :param input_string: the first row contains N - the number of columns
+    defining the landscape of the island. The second line contains N natural
+    numbers — the height of the columns.
+    :return: the number of blocks filled with water
+    """
+    pass
 
 
 def main():
-    # networkx example
-    graph = nx.DiGraph()
-    graph.add_nodes_from([('a', {'color': 'red'}), ('b', {'color': 'red'}),
-                          ('c', {'color': 'red'}), ('d', {'color': 'red'}),
-                          ('e', {'color': 'green'}), ('f', {'color': 'green'}),
-                          ('g', {'color': 'green'})])
-    graph.add_edges_from([('d', 'c'), ('c', 'a'), ('b', 'a'),
-                          ('g', 'f'), ('f', 'e')])
-    print(graph)
-    print('\nVertices of the graph:\n', graph.nodes)
+    print(get_water_volume('11\n2 5 2 3 6 9 3 1 3 4 6'))  # 18
 
-    matrix = nx.adjacency_matrix(graph)
-    print('\nAn adjacency matrix of the graph:\n', matrix.toarray())
-    print(type(matrix.toarray()))
-
-    color_map = [graph.nodes[name]['color'] for name in graph.nodes]
-    nx.draw_planar(graph, with_labels=True, node_color=color_map)
-    plt.show()
-
-    # ScheduleGraph usage
-    tree_count = 3
-    vertex_count = 15
-    schedule_graph = ScheduleGraph.generate_random_forest(tree_count,
-                                                          vertex_count)
-    ScheduleGraph.show_plot(schedule_graph)
-
-    # ScheduleChecker usage
-    if not ScheduleChecker.is_inverted_trees(graph):
-        print('Error! The graph is not a forest with inverted trees!')
-        return
-    print('The graph is a forest with '
-          f'{ScheduleChecker.get_tree_count(graph)} inverted trees!')
-
-    # Schedule usage
-    executor_count = 2
-    schedule = Schedule(graph, executor_count)
-    print(f'Schedule for {executor_count} execotors:')
-    for ex_idx in range(executor_count):
-        print(f'#{ex_idx + 1} executer:\n'
-              f'{schedule.get_schedule_for_executor(ex_idx)}')
-
-    # ScheduleChecker usage
-    print('\nSchedule is checked')
-    errors = ScheduleChecker.get_schedule_errors(schedule.get_schedule())
-    if len(errors) > 0:
-        print('Schedule has errors:\n', '\n'.join(errors))
-    else:
-        print('Schedule has no errors')
+    print(get_win_sequence('4\nMAMA'))  # A
+    print(get_win_sequence('4\nALLOALLO'))  # ALLO
+    print(get_win_sequence('6\nABCOAXLO'))  # ABCOAX
 
 
 if __name__ == '__main__':
---
+++ tests.py
@@ -0,0 +1,71 @@
+import unittest
+import random as rnd
+
+
+from main import get_win_sequence, get_water_volume
+
+
+class TestCase(unittest.TestCase):
+    def test_get_win_sequence(self):
+        test_data = [['4\nMAMA', 'A'],
+                     ['4\nALLOALLO', 'ALLO'],
+                     ['5\nBBABBABB', 'ABB'],
+                     ['6\nABCOAX', 'ABCOAX'],
+                     ['21\nASZYAFAXZEWWAVQHJHBPELYZP', 'AFAXZEWWAVQHJHBPE'],
+                     ['28\nEAUBIYTDBVLMGPDBQFBOLJRVXVVUUSEI',
+                      'AUBIYTDBVLMGPDBQFBOLJRVXVVU']]
+        for input_str, win_str in test_data:
+            self.assertEqual(win_str, get_win_sequence(input_str))
+
+    def test_get_win_sequence_random(self):
+        for _ in range(100):
+            input_str = TestCase.__generate_random_data(1200, 1000)
+            win_str = get_win_sequence(input_str)
+            self.assertTrue(TestCase.__check_win_sequence(input_str, win_str))
+
+    def test_get_water_volume(self):
+        test_data = [['1\n1', 0],
+                     ['2\n1 2', 0],
+                     ['3\n1 2 3', 0],
+                     ['3\n3 2 1', 0],
+                     ['3\n3 1 3', 2],
+                     ['5\n3 1 3 1 5', 4],
+                     ['5\n3 1 3 3 3', 2]]
+        for input_str, volume in test_data:
+            self.assertEqual(volume, get_water_volume(input_str))
+
+    def test_get_water_volume_long_str(self):
+        numbers = ['2'] + ['1'] * 99998 + ['2']
+        long_str = '100000\n' + ' '.join(numbers)
+        self.assertEqual(99998, get_water_volume(long_str))
+
+    @staticmethod
+    def __check_win_sequence(input_string: str, win_seq: str) -> bool:
+        rows = input_string.splitlines()
+        length = int(rows[0])
+        sequence = str(rows[1])
+        if len(win_seq) > length:
+            print('your sequence is longer than the source sequence')
+            return False
+        for i in range(len(win_seq) - 1, -1, -1):
+            offset = length - len(win_seq)
+            if sequence[i + offset] != win_seq[i]:
+                print('your sequence is not a substring of the source sequence')
+                return False
+        for i in range(length):
+            if win_seq > sequence[i:length]:
+                print(f'[{sequence[i:length]}] is less than [{win_seq}]')
+                return False
+        return True
+
+    @staticmethod
+    def __generate_random_data(total_limit: int, target_limit: int) -> str:
+        chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        total_len = rnd.randint(1, total_limit)
+        target_len = rnd.randint(1, min(target_limit, total_len))
+        return (str(target_len) + '\n' +
+                ''.join([rnd.choice(chars) for _ in range(total_len)]))
+
+
+if __name__ == '__main__':
+    unittest.main()
