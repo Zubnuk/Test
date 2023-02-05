2d7d562e1b07341a337fd3fe05976ed96423f97e
Johny314 <113230193+Johny314@users.noreply.github.com>
2023-02-05 22:12:12 +0500

    task_13_Checker

--- schedule_checker.py
+++ schedule_checker.py
@@ -4,14 +4,39 @@ import networkx as nx
 class ScheduleChecker:
     @staticmethod
     def is_inverted_trees(graph: nx.Graph) -> bool:
+        if not nx.is_tree(graph):
+            return False
+        for node in graph.nodes():
+            if graph.degree[node] == 1:
+                continue
+            neighbors = list(graph.neighbors(node))
+            for neighbor in neighbors:
+                if graph.degree[neighbor] != 1:
+                    return False
+        return True
         pass
 
     @staticmethod
     def get_tree_count(graph: nx.Graph) -> int:
+        return graph.number_of_nodes()
         pass
 
     @staticmethod
     def get_schedule_errors(schedule: list[list[str]]) -> list[str]:
+        errors = []
+        task_start_times = {}
+        for node, edges in graph.items(): 
+            for edge in edges:  
+                if node not in task_start_times or edge not in task_start_times: 
+
+                    errors.append(f"Task '{node}' or '{edge}' not found in schedule")  
+
+                elif task_start_times[node] >= task_start_times[edge]:  
+
+                    errors.append(
+                        f"Violation of dependency: Task '{node}' should start before '{edge}'")
+
+        return errors
         pass
 
 def __usage_example():
