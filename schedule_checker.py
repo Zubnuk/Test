78348dd9b2ea3d33b3c8a32a79bc2d7d9f05d512
Gleb <tomlenov16@gmail.com>
2023-02-06 17:21:57 +0500

    .

--- schedule_checker.py
+++ schedule_checker.py
@@ -4,18 +4,71 @@ import networkx as nx
 class ScheduleChecker:
     @staticmethod
     def is_inverted_trees(graph: nx.Graph) -> bool:
-        pass
+        if graph is None or len(graph.nodes) < 1:
+            return False
+        matrix = nx.adjacency_matrix(graph)
+        matrix = matrix.toarray()
+        zero_line_count = 0
+        for i in range(len(matrix)):
+            line_sum = sum(matrix[i])
+            if line_sum > 1:
+                return False
+            elif line_sum == 0:
+                zero_line_count += 1
+        if zero_line_count > 0:
+            return True
+        return False
 
     @staticmethod
     def get_tree_count(graph: nx.Graph) -> int:
-        pass
+        if graph is None or len(graph.nodes) < 1:
+            return 0
+        matrix = nx.adjacency_matrix(graph)
+        matrix = matrix.toarray()
+        zero_line_count = 0
+        for i in range(len(matrix)):
+            if sum(matrix[i]) == 0:
+                zero_line_count += 1
+        if zero_line_count == 0:
+            return 1
+        return zero_line_count
 
     @staticmethod
-    def get_schedule_errors(schedule: list[list[str]]) -> list[str]:
-        pass
+    def get_schedule_errors(graph: nx.Graph, schedule: list[list[str]]) -> list[str]:
+        errors = []
+        all_tasks_in_schedule = []
+
+        for line in schedule:
+            for task in line:
+                all_tasks_in_schedule.append(task)
+                if not list(graph.nodes).count(task) > 0:
+                    errors.append(f'There are no node named {task}')
+
+        for node in list(graph.nodes):
+            if not all_tasks_in_schedule.count(node) > 0:
+                errors.append(f'There are no task named {node}')
+
+        return errors
+
 
 def __usage_example():
-    pass
+    graph = nx.DiGraph()
+    graph.add_nodes_from(['a', 'b'])
+    graph.add_edges_from([('a', 'b')])
+
+    if ScheduleChecker.is_inverted_trees(graph):
+        print('The graph is an inverted trees!')
+    else:
+        print('The graph is NOT an inverted trees!')
+
+    print(f'There are {ScheduleChecker.get_tree_count(graph)} trees in this graph!')
+    schedule = [["a"]]
+    errors = ScheduleChecker.get_schedule_errors(schedule)
+    if len(errors) > 0:
+        print('Schedule has errors:\n', '\n'.join(errors))
+    else:
+        print('Schedule has no errors')
+
 
 if __name__ == '__main__':
     __usage_example()
--- tests/test_schedule_checker.py
+++ tests/test_schedule_checker.py
@@ -1,11 +1,93 @@
 import unittest
+import networkx as nx
 
 from schedule_checker import ScheduleChecker
 
 
 class TestScheduleChecker(unittest.TestCase):
-    def test_something(self):
-        pass
+    def test_empty_graph(self):
+        graph = nx.DiGraph()
+        self.assertFalse(ScheduleChecker.is_inverted_trees(graph))
+        self.assertEqual(ScheduleChecker.get_tree_count(graph), 0)
+
+    def test_none_graph(self):
+        graph = None
+        self.assertFalse(ScheduleChecker.is_inverted_trees(graph))
+        self.assertEqual(ScheduleChecker.get_tree_count(graph), 0)
+
+    def test_one_node(self):
+        graph = nx.DiGraph()
+        graph.add_nodes_from(['a'])
+        self.assertTrue(ScheduleChecker.is_inverted_trees(graph))
+        self.assertEqual(ScheduleChecker.get_tree_count(graph), 1)
+
+    def test_two_nodes_without_edge(self):
+        graph = nx.DiGraph()
+        graph.add_nodes_from(['a', 'b'])
+        self.assertTrue(ScheduleChecker.is_inverted_trees(graph))
+        self.assertEqual(ScheduleChecker.get_tree_count(graph), 2)
+
+    def test_two_nodes_with_edge(self):
+        graph = nx.DiGraph()
+        graph.add_nodes_from(['a', 'b'])
+        graph.add_edges_from([('a', 'b')])
+        self.assertTrue(ScheduleChecker.is_inverted_trees(graph))
+        self.assertEqual(ScheduleChecker.get_tree_count(graph), 1)
+
+    def test_two_nodes_looped(self):
+        graph = nx.DiGraph()
+        graph.add_nodes_from(['a', 'b'])
+        graph.add_edges_from([('a', 'b'), ('b', 'a')])
+        self.assertFalse(ScheduleChecker.is_inverted_trees(graph))
+        self.assertEqual(ScheduleChecker.get_tree_count(graph), 1)
+
+    def test_many_nodes_and_edges(self):
+        graph = nx.DiGraph()
+        graph.add_nodes_from([('a', {'color': 'red'}), ('b', {'color': 'red'}),
+                              ('c', {'color': 'red'}), ('d', {'color': 'red'}),
+                              ('e', {'color': 'green'}), ('f', {'color': 'green'}),
+                              ('g', {'color': 'green'})])
+        graph.add_edges_from([('d', 'c'), ('c', 'a'), ('b', 'a'),
+                              ('g', 'f'), ('f', 'e')])
+        self.assertTrue(ScheduleChecker.is_inverted_trees(graph))
+        self.assertEqual(ScheduleChecker.get_tree_count(graph), 2)
+
+    def test_many_nodes_with_loop(self):
+        graph = nx.DiGraph()
+        graph.add_nodes_from([('a', {'color': 'red'}), ('b', {'color': 'red'}),
+                              ('c', {'color': 'red'}), ('d', {'color': 'red'}),
+                              ('e', {'color': 'green'}), ('f', {'color': 'green'}),
+                              ('g', {'color': 'green'})])
+        graph.add_edges_from([('d', 'c'), ('c', 'a'), ('b', 'a'),
+                              ('g', 'f'), ('f', 'e'), ('d', 'b')])
+        self.assertFalse(ScheduleChecker.is_inverted_trees(graph))
+        self.assertEqual(ScheduleChecker.get_tree_count(graph), 2)
+
+    def test_two_nodes_with_one_error(self):
+        graph = nx.DiGraph()
+        graph.add_nodes_from(['a', 'b'])
+        graph.add_edges_from([('a', 'b')])
+        schedule = [["a"]]
+        self.assertEqual(ScheduleChecker.get_schedule_errors(graph, schedule), ['There are no task named b'])
+
+    def test_many_nodes_many_errors(self):
+        graph = nx.DiGraph()
+        graph.add_nodes_from([('a', {'color': 'red'}), ('b', {'color': 'red'}),
+                              ('c', {'color': 'red'}), ('d', {'color': 'red'}),
+                              ('e', {'color': 'green'}), ('f', {'color': 'green'}),
+                              ('g', {'color': 'green'})])
+        graph.add_edges_from([('d', 'c'), ('c', 'a'), ('b', 'a'),
+                              ('g', 'f'), ('f', 'e')])
+        schedule = [['k', 'd'], ['c', 'b'], ['g', 'a'], ['f']]
+        self.assertEqual(ScheduleChecker.get_schedule_errors(graph, schedule), ['There are no node named k',
+                                                                                'There are no task named e'])
+
+    def test_two_nodes_no_errors(self):
+        graph = nx.DiGraph()
+        graph.add_nodes_from(['a', 'b'])
+        graph.add_edges_from([('a', 'b')])
+        schedule = [["a"], ['b']]
+        self.assertEqual(ScheduleChecker.get_schedule_errors(graph, schedule), [])
 
 
 if __name__ == '__main__':
