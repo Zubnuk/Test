108cce876d477e8ee530f7eb55b613dc6538cc6c
Daniil Svidler <svidlerdaniil@yandex.ru>
2023-02-06 14:19:08 +0500

    функции реализованы

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ def get_win_sequence(input_string: str) -> str:
     Latin letters.
     :return: a subsequence from the source sequence.
     """
-    pass
+    strings = []
+    n, st = input_string.split("\n")
+    n = int(n)
+    strings.append(st[:n])
+    for i in range(len(strings[0]) - 1):
+        strings.append(strings[i][1:])
+    return sorted(strings)[0]
 
 
 def get_water_volume(input_string: str) -> int:
@@ -17,7 +23,32 @@ def get_water_volume(input_string: str) -> int:
     numbers — the height of the columns.
     :return: the number of blocks filled with water
     """
-    pass
+    n, st = input_string.split("\n")
+    st = [int(a) for a in st.split()]
+    print(st)
+    n = int(n)
+    maximum = -1
+    maxes_first = []
+    maxes_second = []
+
+    for i in range(len(st)):
+        if st[i] > maximum:
+            maximum = st[i]
+            maxes_first.append(maximum)
+        else:
+            maxes_first.append(maximum)
+    maximum = -1
+    for i in range(len(st) - 1, -1, -1):
+        if st[i] > maximum:
+            maximum = st[i]
+            maxes_second.append(maximum)
+        else:
+            maxes_second.append(maximum)
+    maxes_second = maxes_second[::-1]
+    differences = []
+    for i in range(len(maxes_first)):
+        differences.append(min(maxes_first[i], maxes_second[i]) - st[i])
+    return (sum(differences))
 
 
 def main():
