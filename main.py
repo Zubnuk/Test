b3c8a5647d4c57c04293f3a7d3c25236960bb2d9
andrejsmirnov <aosmirnov_4@edu.hse.ru>
2023-02-07 18:44:45 +0500

    ver 1

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ def get_win_sequence(input_string: str) -> str:
     Latin letters.
     :return: a subsequence from the source sequence.
     """
-    pass
+    Str = input_string.split("\n")
+    data = [Str[1][:int(Str[0])]]
+    for i in range(len(data[0]) - 1):
+        data.append(data[i][1:])
+    return sorted(data)[0]
 
 
 def get_water_volume(input_string: str) -> int:
@@ -17,15 +21,29 @@ def get_water_volume(input_string: str) -> int:
     numbers — the height of the columns.
     :return: the number of blocks filled with water
     """
-    pass
+    Str = input_string.split("\n")
+    data = list(map(int, Str[1].split(' ')))
+    L, R = 0, len(data) - 1
+    Max_L, Max_R = data[L], data[R]
+    result = 0
+    while L < R:
+        if Max_L >= Max_R:
+            result += Max_R - data[R]
+            R -= 1
+            Max_R = max(Max_R, data[R])
+        else:
+            result += Max_L - data[L]
+            L += 1
+            Max_L = max(Max_L, data[L])
+    return result
 
 
 def main():
     print(get_water_volume('11\n2 5 2 3 6 9 3 1 3 4 6'))  # 18
 
-    print(get_win_sequence('4\nMAMA'))  # A
-    print(get_win_sequence('4\nALLOALLO'))  # ALLO
-    print(get_win_sequence('6\nABCOAXLO'))  # ABCOAX
+    # print(get_win_sequence('4\nMAMA'))  # A
+    # print(get_win_sequence('8\nALLOALLO'))  # ALLO
+    # print(get_win_sequence('21\nASZYAFAXZEWWAVQHJHBPELYZP'))  # AFAXZEWWAVQHJHBPE
 
 
 if __name__ == '__main__':
